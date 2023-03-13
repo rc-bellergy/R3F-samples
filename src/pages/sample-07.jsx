5c1a9bc51bb694af02eb3a1b45ec93e388bb1e25
Bellergy RC <rc@bellergy.com>
2023-03-13 21:53:39 +0800

    update sample 07

--- src/pages/sample-07.jsx
+++ src/pages/sample-07.jsx
@@ -1,8 +1,10 @@
 import * as THREE from "three"
-import { OrbitControls, Environment, Html, PivotControls } from '@react-three/drei'
+import { OrbitControls, Environment, Html, PivotControls, Center } from '@react-three/drei'
 import { Suspense, useRef, useState } from 'react'
 import { Model as Iphone13Pro } from '../components/Iphone_13_pro_mod.jsx'
 import Loading from '../components/Loading.jsx'
+import DefaultGrid from '../components/DefaultGrid.jsx'
+
 
 // References:
 // Understanding quaternions
@@ -12,7 +14,7 @@ export default function Sample07() {
 
     const iPhoneRef = useRef()
 
-    const [data, setData] = useState('')
+    const [data, setData] = useState(new THREE.Quaternion())
 
     return <>
         <Suspense fallback={<Loading text='Loading iPhone' />} >
@@ -24,12 +26,17 @@ export default function Sample07() {
                     const quaternion = new THREE.Quaternion()
                     const scale = new THREE.Vector3()
                     w.decompose(position, quaternion, scale)
-                    setData(`x:${quaternion.x}, y:${quaternion.y}, z:${quaternion.z}, w:${quaternion.w}`)
+                    setData(quaternion)
                 }}>
                 <Iphone13Pro scale={1.5} />
             </PivotControls>
         </Suspense>
-        {/* <OrbitControls makeDefault /> */}
+
+        <Center bottom>
+            <DefaultGrid />
+        </Center>
+
+        <OrbitControls makeDefault />
         <Environment preset="city" />
 
         {/* Descriptions */}
@@ -39,8 +46,11 @@ export default function Sample07() {
                 <li>PivotControls</li>
                 <li>Extract rotation from PivotControls</li>
             </ul>
-            <h3>quaternion:</h3>
-            <p>{data}</p>
+            <p>quaternion:</p>
+            <p> {`x:${data.x}`}<br/>
+                {`y:${data.y}`}<br/>
+                {`z:${data.z}`}<br/>
+                {`w:${data.w}`}</p>
         </Html>
 
     </>
